e148c7ebf0af8b992b3f434ac0c647e7d9bb1881
Stefan Huber <Stefan Huber@172.19.0.210>
2016-03-05 09:26:28 +0100

    Removed unused import

--- Grün2_Mitgliederverwaltung/src/bayern/steinbrecher/gruen2/elements/ConfirmDialog.java
+++ Grün2_Mitgliederverwaltung/src/bayern/steinbrecher/gruen2/elements/ConfirmDialog.java
@@ -5,7 +5,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
