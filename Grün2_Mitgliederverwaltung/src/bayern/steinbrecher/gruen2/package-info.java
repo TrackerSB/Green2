97e99b35d89b134de4f1fddc5cb4630a3fcb414c
Stefan Huber <Stefan Huber@172.19.0.210>
2016-04-12 17:38:09 +0200

    Added documentation for optional column Beitrag.

--- Grün2_Mitgliederverwaltung/src/bayern/steinbrecher/gruen2/package-info.java
+++ Grün2_Mitgliederverwaltung/src/bayern/steinbrecher/gruen2/package-info.java
@@ -5,30 +5,35 @@
  * attributes.
  * <ol>
  * <li>Mitglieder
- *     <ul>
- *     <li>Mitgliedsnummer (primary key)</li>
- *     <li>Titel</li>
- *     <li>Vorname</li>
- *     <li>Nachname</li>
- *     <li>istMaennlich (0=women, 1=men)</li>
- *     <li>Strasse</li>
- *     <li>Hausnummer</li>
- *     <li>PLZ</li>
- *     <li>Ort</li>
- *     <li>AusgetretenSeit (0000-00-00 if not resigned)</li>
- *     <li>IBAN</li>
- *     <li>BIC</li>
- *     <li>MandatErstellt (The date when the member signed the direct debit)</li>
- *     <li>KontoinhaberVorname (Only needed when Vorname is different from the prename of the account holder)</li>
- *     <li>KontoinhaberNachname (Only needed when Nachname is different from the lastname of the account holder)</li>
- *     <li>istBeitragsfrei (0=has to pay contribution, 1=has not to pay contribution)</li>
- *     </ul>
+ * <ul>
+ * <li>Mitgliedsnummer (primary key)</li>
+ * <li>Titel</li>
+ * <li>Vorname</li>
+ * <li>Nachname</li>
+ * <li>istMaennlich (0=women, 1=men)</li>
+ * <li>Strasse</li>
+ * <li>Hausnummer</li>
+ * <li>PLZ</li>
+ * <li>Ort</li>
+ * <li>AusgetretenSeit (0000-00-00 if not resigned)</li>
+ * <li>IBAN</li>
+ * <li>BIC</li>
+ * <li>MandatErstellt (The date when the member signed the direct debit)</li>
+ * <li>KontoinhaberVorname (Only needed when Vorname is different from the
+ * prename of the account holder)</li>
+ * <li>KontoinhaberNachname (Only needed when Nachname is different from the
+ * lastname of the account holder)</li>
+ * <li>istBeitragsfrei (0=has to pay contribution, 1=has not to pay
+ * contribution)</li>
+ * <li>Beitrag (Optional. Used for setting individual contributions. If not
+ * specified Grün2 asks for a default contribution)</li>
+ * </ul>
  * </li>
  * <li>Spitznamen
- *     <ul>
- *     <li>Name (The real name; primary key)</li>
- *     <li>Spitzname (The associated name)</li>
- *     </ul>
+ * <ul>
+ * <li>Name (The real name; primary key)</li>
+ * <li>Spitzname (The associated name)</li>
+ * </ul>
  * </li>
  * </ol>
  *
