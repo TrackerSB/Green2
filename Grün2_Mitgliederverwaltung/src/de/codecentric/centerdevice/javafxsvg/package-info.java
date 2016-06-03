c75962a48e18244ebebc1d2536c8fa56e8a04886
Stefan Huber <huberst@fim.uni-passau.de>
2016-06-03 10:21:21 +0200

    Added package info.

---
+++ Grün2_Mitgliederverwaltung/src/de/codecentric/centerdevice/javafxsvg/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains classes providing SVG support.
+ * @see https://github.com/codecentric/javafxsvg
+ * @see https://xmlgraphics.apache.org/batik/download.html
+ */
+package de.codecentric.centerdevice.javafxsvg;
