a01a3143fb637e45d61a91bbc3ac35df377658b1
Stefan Huber <huberst@fim.uni-passau.de>
2016-06-22 15:40:54 +0200

    corrected install.sh

--- Installationstest/install.sh
+++ Installationstest/install.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
+# Determine sudo command
+KdeSudoExists=$(command -v kdesudo >/dev/null)
+GkSudoExists=$(command -v gksudo >/dev/null)
+if ! ($KdeSudoExists || $GkSudoExists)
+then
+    >&2 echo "You have to install kdesudo or gksudo"
+    return 1
+else
+    if $KdeSudoExists
+    then
+        SudoCommand=kdesudo
+    else
+        SudoCommand=gksudo
+    fi
+fi
+
+# Set variables
 TempDir=$(dirname $0)
 ConfigDir="$HOME/.Grün2_Mitgliederverwaltung"
 ProgramFolder="/opt/Grün2_Mitgliederverwaltung"
 
+
+# Create folders
+$SudoCommand mkdir $ProgramFolder
 mkdir "$ConfigDir"
-sudo mkdir $ProgramFolder
 
 # Move config file
 cp $TempDir/Grün2.conf $ConfigDir/
 
 # Move program itself
-sudo cp -r $TempDir/* $ProgramFolder/
+$SudoCommand -c "cp -r $TempDir/* $ProgramFolder/"
 
 # Remove xml and bat files which are only used in windows
-sudo rm $ProgramFolder/*.xml
-sudo rm $ProgramFolder/*.bat
+$SudoCommand rm $ProgramFolder/*.xml
+$SudoCommand rm $ProgramFolder/*.bat
 
 echo "Grün2 wurde installiert/aktualisiert"
