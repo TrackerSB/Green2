8da4e4a062d5f4b50ecb404c49d2c5063b001e5c
Stefan Huber <huberst@fim.uni-passau.de>
2016-07-10 14:28:41 +0200

    Removed unused import

--- Grün2_Mitgliederverwaltung/src/bayern/steinbrecher/gruen2/elements/Splashscreen.java
+++ Grün2_Mitgliederverwaltung/src/bayern/steinbrecher/gruen2/elements/Splashscreen.java
@@ -8,7 +8,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
