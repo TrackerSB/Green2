e7ded48a4122696e280fd94db8d690ec8eecd3be
Stefan Huber <huberst@fim.uni-passau.de>
2016-09-07 19:59:55 +0200

    Removed getAdminrights function of batch file.

--- Installationstest/install.bat
+++ Installationstest/install.bat
@@ -16,20 +16,3 @@ if not exist "%ProgramFilesPath%" (
 cscript /nologo "%~dp0install.vbs"
 
 goto :eof
-
-
-:getAdminrights
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\icacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-set ADMINSCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
-REM --> If error flag set, we do not have admin.
-if '%ERRORLEVEL%' NEQ '0' (
-    echo Set UAC = CreateObject^("Shell.Application"^) > %ADMINSCRIPT%
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> %ADMINSCRIPT%
-
-    cscript /nologo %ADMINSCRIPT%
-    del %ADMINSCRIPT%
-)
-goto :eof
