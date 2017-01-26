b1f4a336b2cd265ecc3730c497b46c4e579cca5c
TrackerSB <junktogo@web.de>
2017-01-26 09:50:45 +0100

    Updated copyright

--- DataProvider/src/bayern/steinbrecher/green2/WizardableController.java
+++ DataProvider/src/bayern/steinbrecher/green2/WizardableController.java
@@ -3,6 +3,7 @@
  * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
  */
+
 package bayern.steinbrecher.green2;
 
 import javafx.beans.property.BooleanProperty;
