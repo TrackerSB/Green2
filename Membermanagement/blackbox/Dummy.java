93b6d0dc280df05f6564742b43610ca1870a17ce
TrackerSB <junktogo@web.de>
2017-02-19 10:32:54 +0100

    Dummy class for making git adding the empty blackbox directory needed for executing tests successfully.

---
+++ Membermanagement/blackbox/Dummy.java
@@ -0,0 +1,38 @@
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ * This is just a dummy class needed in order to make git add the blackbox directory.
+ *
+ * @author Stefan Huber
+ */
+public class Dummy {
+
+    public Dummy() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
