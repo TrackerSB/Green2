f25750ae4b7a2e76726c8d5da1a63734eabf9ee3
Stefan Huber <junktogo@web.de>
2017-11-28 14:49:58 +0100

    Rename InstallHandler to pure Uninstaller (#37, #38)

---
+++ ParentExecutables/Uninstaller/src/main/java/bayern/steinbrecher/green2/uninstaller/Uninstaller.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017. Stefan Huber
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package bayern.steinbrecher.green2.uninstaller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class is only needed for the installer to determine where to put the version of Green2.
+ *
+ * @author Stefan Huber
+ */
+public final class Uninstaller extends Application {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        throw new UnsupportedOperationException("The uninstall process is not yet implemented.");
+    }
+
+    /**
+     * The main method.
+     *
+     * @param args The commandline arguments.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
