1a295a4f73652b6fbc72090760ec49ffe490eb2b
Stefan Huber <junktogo@web.de>
2018-02-17 10:56:45 +0100

    Correct setting the results in result view (#58)

--- ParentExecutables/MemberManagement/src/main/java/bayern/steinbrecher/green2/query/QueryResultController.java
+++ ParentExecutables/MemberManagement/src/main/java/bayern/steinbrecher/green2/query/QueryResultController.java
@@ -18,9 +18,13 @@ package bayern.steinbrecher.green2.query;
 
 import bayern.steinbrecher.green2.WizardableController;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -34,7 +38,7 @@ import javafx.scene.layout.Priority;
 public class QueryResultController extends WizardableController {
 
     @FXML
-    private TableView queryResultView;
+    private TableView<List<ReadOnlyStringProperty>> queryResultView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -42,19 +46,38 @@ public class QueryResultController extends WizardableController {
     }
 
     public void setQueryResult(List<List<String>> queryResult) {
-        //TODO Check length of each row
         queryResultView.getItems().clear();
         queryResultView.getColumns().clear();
-        List<String> headings = queryResult.get(0);
-        for (int i = 0; i < headings.size(); i++) {
-            String heading = headings.get(i);
-            TableColumn column = new TableColumn(heading);
-            column.setCellValueFactory(param -> {
-                return param;
-            });
-            queryResultView.getColumns().add(column);
+
+        if (queryResult.size() > 0) {
+            int numColumns = queryResult.stream()
+                    .mapToInt(List::size)
+                    .max()
+                    .orElse(0);
+            List<String> headings = queryResult.get(0);
+            for (int i = 0; i < numColumns; i++) {
+                String heading = i >= headings.size() ? "" : headings.get(i);
+                TableColumn<List<ReadOnlyStringProperty>, String> column = new TableColumn<>(heading);
+                final int fixedI = i;
+                column.setCellValueFactory(param -> param.getValue().get(fixedI));
+                queryResultView.getColumns().add(column);
+            }
+
+            if (queryResult.size() > 1) {
+                //TODO Is there a way to "collect" to an observable list?
+                ObservableList<List<ReadOnlyStringProperty>> items = FXCollections.observableArrayList();
+                queryResult.subList(1, queryResult.size()).stream()
+                        .map(givenRow -> {
+                            List<ReadOnlyStringProperty> itemsRow = new ArrayList<>(numColumns);
+                            for (int i = 0; i < numColumns; i++) {
+                                String cellValue = i >= givenRow.size() ? "" : givenRow.get(i);
+                                itemsRow.add(new SimpleStringProperty(cellValue));
+                            }
+                            return itemsRow;
+                        })
+                        .forEach(itemsRow -> items.add(itemsRow));
+                queryResultView.setItems(items);
+            }
         }
-        //TODO Create ObservableList of ObservableList
-        queryResultView.setItems(FXCollections.observableList(headings.subList(1, headings.size())));
     }
 }
