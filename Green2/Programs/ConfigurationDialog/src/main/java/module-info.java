bee73a94ab2eb35c22dde294a6ca4fd944e4cf5b
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-09-19 07:35:08 +0200

    Nailcare to module dependency ordering

--- Green2/Programs/ConfigurationDialog/src/main/java/module-info.java
+++ Green2/Programs/ConfigurationDialog/src/main/java/module-info.java
@@ -1,11 +1,10 @@
 module bayern.steinbrecher.green2.ConfigurationDialog {
     exports bayern.steinbrecher.green2.configurationDialog;
 
-    requires bayern.steinbrecher.green2.SharedBasis;
-
     requires bayern.steinbrecher.CheckedElements;
     requires bayern.steinbrecher.DBConnector;
     requires bayern.steinbrecher.GenericWizard;
+    requires bayern.steinbrecher.green2.SharedBasis;
     requires bayern.steinbrecher.Utility;
     requires java.logging;
     requires javafx.controls;
--- Green2/Programs/Launcher/src/main/java/module-info.java
+++ Green2/Programs/Launcher/src/main/java/module-info.java
@@ -3,8 +3,8 @@ module bayern.steinbrecher.green2.Launcher {
 
     requires bayern.steinbrecher.CheckedElements;
     requires bayern.steinbrecher.GenericWizard;
-    requires bayern.steinbrecher.Utility;
     requires bayern.steinbrecher.green2.SharedBasis;
+    requires bayern.steinbrecher.Utility;
     requires java.compiler;
     requires java.logging;
     requires java.xml;
